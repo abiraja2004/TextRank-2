ca057799f896c993e225a5e399b9a5cc3948a024
Prakhar Mishra <pmprakhargenius@gmail.com>
2016-09-16 00:25:26 +0530

    parameter to draw graph

--- run_textRank.sh
+++ run_textRank.sh
@@ -5,4 +5,4 @@ echo "=========================="
 echo "Sentences - 2"
 echo "Weight Metric - Levenshtein"
 echo "=========================="
-python textRank.py -s 2 -d L
+python textRank.py -s 2 -d L -draw T
