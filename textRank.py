0bce0543002da0b25c6c386c31bf329917545987
Prakhar Mishra <pmprakhargenius@gmail.com>
2016-09-20 00:25:15 +0530

    Keyword Extraction added

--- textRank.py
+++ textRank.py
@@ -19,7 +19,11 @@ import argparse
 import itertools
 import math
 import matplotlib.pyplot as plt
-
+from scipy import spatial
+import gensim
+import numpy as np
+from slidingWindow import SlidingWindow
+np.set_printoptions(threshold=np.nan)
 
 # Distance Metric 1
 def LavenDistance(firstString, secondString):
@@ -51,27 +55,29 @@ def CommonTokens(string_1, string_2, graph):
     similarity =  similar_tokens / float(math.log(string_length_1) + math.log(string_length_2))
 
     return similarity
-    """
-    # Remove nodes for which no similar/common words/tokens are found
-    if similar_tokens != 0:
-        similarity = similar_tokens / float(math.log(string_length_1) + math.log(string_length_2))
-    else:
-	graph.remove_node(string_1)
-	graph.remove_node(string_2)
-   	similarity = 0 
-    return similarity,graph
-    """
-  
-def CosineSimilarity(string_vec_1, string_vec_2):
-    pass
+ 
+
+def getVector(string_1, string_2, model):
+    #return string_1, string_2
+    vec1 = model[string_1][0]
+    vec2 = model[string_2][0]
+    return vec1, vec2
+
+def CosineSimilarity(string_1, string_2, model):
+    string_1_vec, string_2_vec = getVector(string_1, string_2, model)
+    #return [string_1_vec, string_2_vec]
+    r = 1 - spatial.distance.cosine(string_1_vec, string_2_vec)
+    return r    
+
 
 def computeTfIdf(nodes_list):
     pass
 
 
 # Graph generation
-def generateGraph(nodes_list, distanceMetric):
- 
+def generateGraph(nodes_list, distanceMetric, model):
+
+    
     # Initialize of an graph
     gr = nx.Graph() 
     gr.add_nodes_from(nodes_list)
@@ -97,14 +103,14 @@ def generateGraph(nodes_list, distanceMetric):
 	    gr.add_edge(string_1, string_2, weight=commonTokenCount)
         return gr
    
-    elif distanceMetric == 'CS':
-	computeTfIdf(nodes_list)
-	for node in nodePairs:
+    elif distanceMetric == 'w2vCS':
+        for node in nodePairs:
 	    string_1 = node[0]
 	    string_2 = node[1]
-            #add edges to graph (weughteed by cosine similarity)
-	    cosineScore = CosineSimilarity(string_1, string_2)
-	    gr.add_edge(string_1,string_2,weight=cosineScore,time=str(cosineScore))
+	    cosineSimilarity = CosineSimilarity(string_1, string_2, model)
+	    #return cosineSimilarity
+	    gr.add_edge(string_1, string_2, weight=cosineSimilarity)
+	
 	return gr
 
 
@@ -118,16 +124,30 @@ def cleanText(text):
         stopRemoved = ' '.join([w for w in s.split() if w not in stopModList])
         puncRemoved = ''.join([w for w in stopRemoved if w not in punctuation])
         s_f.append(puncRemoved)
-    
     return s_f
     
 
 # Draw the graph
-def drawGraph(graph):
+def drawGraph(G):
 	try:
-	    #nx.draw_networkx_edge_labels(graph,pos,edge_labels=edge_weight)
-	    nx.draw_circular(graph)
+	    pos = nx.spring_layout(G)
+	    #nx.draw_circular(G, with_labels = True)
+	    #elarge=[(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] > 2]
+	    #esmall=[(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] <= 2]
+	    edge_weight=dict([((u,v,),int(d['weight'])) for u,v,d in G.edges(data=True)])
+	    #nx.draw_networkx_edges(G,pos,edgelist=elarge, width=6)
+	    #nx.draw_networkx_edges(G,pos,edgelist=esmall, width=6,alpha=0.2,style='dashed')
+
+	    nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_weight)
+	    nx.draw_networkx_nodes(G,pos)
+	    nx.draw_networkx_edges(G,pos)
+	    nx.draw_networkx_labels(G,pos)
+	    plt.axis('off')
 	    plt.show()
+
+	    #nx.draw_networkx_edge_labels(graph,pos,edge_labels=edge_weight)
+	    #nx.draw_circular(graph)
+	    #plt.show()
 	except Exception as e:
 		return 0
 	return 1
@@ -142,7 +162,7 @@ def extractSummary(text, sent, dist, draw='F'):
     s_f = cleanText(sentenceTokens)
 
     graph = generateGraph(s_f,dist)
-    
+    return graph 
     if draw != 'F':
         if drawGraph(graph):
             pass
@@ -152,7 +172,7 @@ def extractSummary(text, sent, dist, draw='F'):
     else:
         pass
 
-    calculated_page_rank = nx.pagerank(graph, weight='weight')
+    calculated_page_rank = nx.pagerank(graph, max_iter=100,weight='weight')
     #return calculated_page_rank
     #most important sentences in ascending order of importance
     sentences = sorted(calculated_page_rank, key=calculated_page_rank.get, reverse=True)
@@ -164,6 +184,151 @@ def extractSummary(text, sent, dist, draw='F'):
 
     return summary
 
+
+def removeDuplicates(taggedTokens):
+    L = []
+    for i in taggedTokens:
+        if i not in L:
+	    L.append(i)
+	else:
+            pass
+
+    return L
+
+
+def generateCoocurrenceMatrix(taggedTokens,windowLength):
+    i = 0
+    matrix_row = matrix_col = len(taggedTokens)
+    w = []
+    # First considering a fully connected graph
+    # Filling diagonal entries to be `ZERO` {ignoring self loop}
+    mat = np.zeros((matrix_row, matrix_col), int)
+
+    # Matrix of `matrix_row` X `matrix_col` will be made
+    # Logic to fill in the cell with co-occurence distance
+    sliding_w = SlidingWindow(windowLength, taggedTokens)	
+
+    for i in range(len(taggedTokens)):
+        prev, curr, nxt = sliding_w[i][0], sliding_w[i][1], sliding_w[i][2]
+	for _i in prev:
+	    w.append(_i)
+	w.append(curr)
+	for i_ in nxt:
+	    w.append(i_)
+	
+	for j in range(len(taggedTokens)):
+	    if i==j:
+	        mat[i][j] = 0
+            else:
+	        if taggedTokens[j] in w:
+		    distance = abs(w.index(taggedTokens[j]) - w.index(taggedTokens[i]))
+		    mat[i][j] = distance
+        w = []
+    return mat
+
+
+def makeTokenGraph(nodePairsAndWeight):
+    gr = nx.Graph()
+    for i in nodePairsAndWeight:
+        gr.add_node(i[0])
+	gr.add_node(i[1])
+	gr.add_edge(i[0],i[1])
+	gr[i[0]][i[1]]['weight'] = i[2]
+
+    return gr
+
+
+
+def postProcessingKeywords(words,wordTokens):
+    j = 1
+    kPhrase = ''
+    Phrases = []
+    
+    while j < len(wordTokens):
+        fWord = wordTokens[j-1]
+	sWord = wordTokens[j]
+	
+	if fWord in words and sWord in words:
+	    kPhrase = fWord + ' '  + sWord     
+            Phrases.append(kPhrase)
+	    words.remove(fWord)
+	    words.remove(sWord)
+	j += 1
+
+    for i in words:
+        Phrases.append(i)
+    return Phrases
+
+def extractKeywords(text,dist,w, draw, window=11):
+    
+    text1 = text.lower()
+
+    # Clean the input 
+    # `[text]` because `cleanText` accepts that format
+    # Also tokenize it 
+    text = cleanText([text1])
+    wordTokens = nltk.word_tokenize(text[0]) 
+    
+    # Do POS Tagging and extract Nouns and Adjectives
+    taggedTokens = nltk.pos_tag(wordTokens)
+    taggedTokens = [i[0] for i in taggedTokens if i[1] in ["NN","JJ","NNP","NNS","JJS","JJR","NNPS"]]
+     
+    #return taggedTokens   
+    # Remove the duplicate ones | Not required here
+    uniqueTokens = removeDuplicates(taggedTokens)
+  
+    #return uniqueTokens 
+    # Window size over which co-occurence matrix will be generated
+    # Sliding Window of length of 5 means [w1,w2,w3,w4,w5,W,w6,w7,w8,w9,w10]
+    windowSize = window
+    mat = generateCoocurrenceMatrix(uniqueTokens,windowSize) 
+    #return mat
+    # Node pairs that will make the graph
+    n1,n2 = np.nonzero(mat)[0],np.nonzero(mat)[1]
+    nodePairs = [(node1,node2) for node1,node2 in zip(n1,n2)]
+    
+    # Weight for all the connected node pairs
+    weightList = []
+    for n in nodePairs:
+        w = mat[n[0]][n[1]]
+	weightList.append(w)
+    
+    # Nodes in graph(pair) and their connected weight
+    nodePairsAndWeight = [(node1,node2,weight) for node1,node2,weight in zip(n1,n2,weightList)] 
+    #return nodePairsAndWeight
+    print "Generating graph...\n"
+    graph = makeTokenGraph(nodePairsAndWeight)
+   
+    if draw != 'F':
+        if drawGraph(graph):
+	    pass
+	else:
+            print 'Failed to draw\n'
+	    sys.exit()
+    else:
+	    pass
+    
+
+    # Apply PageRank
+    calculated_page_rank = nx.pagerank(graph, weight='weight')
+    #return calculated_page_rank
+    #return uniqueTokens
+    
+     
+    words = sorted(calculated_page_rank, key=calculated_page_rank.get, reverse=True)
+    #return words
+    #return words
+    # Joining the keywords : if keywords selected occcur adjacent to eachother in the original text
+    
+    words_w = [uniqueTokens[i] for i in words[:30]]
+    text1 = "".join([i for i in text1 if i not in punctuation])
+
+    words_w_f = postProcessingKeywords(words_w,text1.split())
+    return words_w_f
+    #return [uniqueTokens[i] for i in words[:10]]
+    #number_words = int(w)
+    
+    #return words[:number_words]
 
 def readCommandLineArguments():
     parser = argparse.ArgumentParser(description='Auto Summarization of text')
@@ -178,9 +343,11 @@ def readCommandLineArguments():
 if __name__=="__main__":
     
     #`Example 1`
-    #text = requests.get('http://rare-technologies.com/the_matrix_synopsis.txt').text
-    
+    #text = requests.get('http://rare-technologies.com/the_big_lebowski_synopsis.txt').text
+    #print text
     text = open('data.txt','r').read()
+    
     sent, dist ,draw = readCommandLineArguments()
-    summary = extractSummary(text.decode('utf-8'),sent, dist,draw)
-    print summary
+    keywords = extractKeywords(text.decode('utf-8'),dist,sent, draw)
+    #summary = extractSummary(text.decode('utf-8'),sent, dist,draw)
+    print keywords
